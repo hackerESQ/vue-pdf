a850c7a80d02e70db9e0235fb104b506e2a208e1
hackerESQ <corey@coreyvarma.com>
2021-03-29 23:02:37 -0500

    Update vuePdfNoSss.vue

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -1,17 +1,22 @@
 <style src="./annotationLayer.css"></style>
 <script>
+console.log('test-1')
 
 	import componentFactory from './componentFactory.js'
 
+console.log('test0')
 	if ( process.env.VUE_ENV !== 'server' ) {
-
+console.log('test1')
 		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
 		var PDFJS = require('pdfjs-dist/es5/build/pdf.js');
 
 		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-
-			var PdfjsWorker = require('worker-loader!pdfjs-dist/es5/build/pdf.worker.js');
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
+console.log('test2')
+			var PdfjsWorker = require('pdfjs-dist/es5/build/pdf.worker.js');
+			PDFJS.GlobalWorkerOptions.isWorkerDisabled = true;
+			PDFJS.GlobalWorkerOptions.workerSrc = '/service.worker.js';
+			
+			// PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
 		}
 
 		var component = componentFactory(pdfjsWrapper(PDFJS));
