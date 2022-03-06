f67ff2ded9702c84eb823b76d245660f1c1a316b
hackerESQ <corey@coreyvarma.com>
2022-03-06 13:00:56 -0600

    Update vuePdfNoSss.vue

--- src/vuePdfNoSss.vue
+++ src/vuePdfNoSss.vue
@@ -3,23 +3,20 @@
 
 	import componentFactory from './componentFactory.js'
 
-	if ( process.env.VUE_ENV !== 'server' ) {
-		var pdfjsWrapper = require('./pdfjsWrapper.js').default;
-		var PDFJS = require('pdfjs-dist/build/pdf.js');
 
-		if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
-			var PdfjsWorker = require('pdfjs-dist/build/pdf.worker.js');
-			// PDFJS.GlobalWorkerOptions.isWorkerDisabled = true;
-			// PDFJS.GlobalWorkerOptions.workerSrc = '/service.worker.js';
-			
-			PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
-		}
+	var pdfjsWrapper = require('./pdfjsWrapper.js').default;
+	var PDFJS = require('pdfjs-dist/build/pdf.js');
 
-		var component = componentFactory(pdfjsWrapper(PDFJS));
-	} else {
+	if ( typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1 ) {
+		var PdfjsWorker = require('pdfjs-dist/build/pdf.worker.js');
+		// PDFJS.GlobalWorkerOptions.isWorkerDisabled = true;
+		// PDFJS.GlobalWorkerOptions.workerSrc = '/service.worker.js';
 
-		var component = componentFactory({});
+		PDFJS.GlobalWorkerOptions.workerPort = new PdfjsWorker();
 	}
+
+	var component = componentFactory(pdfjsWrapper(PDFJS));
+
 
 	export default component;
 </script>
